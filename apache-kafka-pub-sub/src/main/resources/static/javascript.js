6c77fd3f9d9a5824a2364cf7e71c00f1b69e3315
Martin <mjuliom_4@hotmail.com>
2022-08-26 10:59:19 -0300

    Fetch consumer data

--- apache-kafka-pub-sub/src/main/resources/static/javascript.js
+++ apache-kafka-pub-sub/src/main/resources/static/javascript.js
@@ -1,8 +1,31 @@
 
-console.log("Hi")
+console.log("Hi4")
 fetchConsumerData()
 
 function fetchConsumerData() {
     console.log("fetch")
     return fetch("/consumer")
+        .then((response) => {
+            if (response.ok) {   // *** This can be just `if (response.ok) {`
+                return response.json();
+            }
+            else {
+                throw `error with status ${response.status}`;
+            }
+        })
+        .then(json => renderConsumerData(json))
+        .catch(
+            error => {
+                console.log("Error fetch consumer data: " + error)
+            }
+        )
+}
+
+function renderConsumerData(data) {
+    console.log(data)
+    data.forEach( consumer => {
+        console.log(consumer)
+
+    })
+
 }
